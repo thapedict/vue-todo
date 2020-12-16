64032e4f42d4d6bbedcede65839ea07fc9e6c751
Thapelo Moeti <benzitto@gmail.com>
2020-12-16 14:54:01 +0200

    can now add items to list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 new Vue({
     el: '#app',
     data: {
-        message: "Hello, World!"
+        newItem: '',
+        items: []
+    },
+    methods: {
+        addItem: function() {
+            if(this.newItem.length) {
+                this.items.push({
+                    text: this.newItem.toString(),
+                    key: this.items.length
+                });
+                this.newItem = '';
+            }
+        }
     }
 });
