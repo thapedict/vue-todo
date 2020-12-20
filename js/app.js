20f04daf4f5828f13d5b677a16ee7d95cfa2aad0
Thapelo Moeti <benzitto@gmail.com>
2020-12-20 17:26:48 +0200

    added ability to remove items

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 Vue.component('todo-item',{
     props: ['item'],
-    template: '<li>{{item.text}}</li>'
+    template: '<li><span>{{item.text}}</span><button v-on:click="$emit(\'delete-item\',item.key)">X</button></li>',
 });
 new Vue({
     el: '#app',
     data: {
         newItem: '',
+        counter: 0,
         items: []
     },
     methods: {
@@ -13,10 +14,23 @@ new Vue({
             if(this.newItem.length) {
                 this.items.push({
                     text: this.newItem.toString(),
-                    key: this.items.length
+                    key: this.counter++
                 });
                 this.newItem = '';
             }
+        },
+        removeItem: function(key) {
+            let i = getIndex(this.items,key);
+            this.items.splice(i,1);
         }
     }
 });
+
+const getIndex = (arr, key) => {
+    for(let x=0;x<arr.length;x++) {
+        if(arr[x].key==key){
+            return x;
+        }
+    }
+    return false;
+};
